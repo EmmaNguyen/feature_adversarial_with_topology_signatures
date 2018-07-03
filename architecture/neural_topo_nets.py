fa0da7452096b1fcb75aaadd1b644e455c55907c
Emma Nguyen <emma.mphuong@gmail.com>
2018-07-03 16:19:34 -0400

    Refactor run_topo_gan.py (adversarial topological features)
    - Move the piece of training code to architecture, thus make it be
    easier to read
    - Change name of function to make purpose of design clearer

--- architecture/neural_topo_nets.py
+++ architecture/neural_topo_nets.py
@@ -68,98 +68,40 @@ class PHConvNet(torch.nn.Module):
         x = self.linear_2(x)
         return x
 
-class Topo_Generator(nn.Module):
-    def __init__(self, latent_dim, img_size, channels, subscripted_views):
-        super(Generator, self).__init__()
+class ConvNet(nn.Module):
+    def __init__(self, latent_dim, img_size, channels):
+        super(ConvNet, self).__init__()
         self.latent_dim = latent_dim
         self.img_size = img_size
         self.channels = channels
-        self.subscripted_views = subscripted_views
-        self.transform = UpperDiagonalThresholdedLogTransform(0.1)
 
-        def get_init(num_elements):
-            transform = UpperDiagonalThresholdedLogTransform(0.1)
-            return transform(pers_dgm_center_init(num_elements))
-
-        self.dim_0 = SLayer(150, 2, get_init(150), torch.ones(150, 2) * 3)
-        self.dim_0_ess = SLayer(50, 1)
-        self.dim_1_ess = SLayer(50, 1)
-        self.slayers = [self.dim_0,
-                        self.dim_0_ess,
-                        self.dim_1_ess
-                        ]
-
-        self.stage_1 = []
-        stage_1_outs = [75, 25, 25]
-
-        for i, (n_in, n_out) in enumerate(zip([150, 50, 50], stage_1_outs)):
-            seq = nn.Sequential()
-            seq.add_module('linear_1', nn.Linear(n_in, n_out))
-            seq.add_module('batch_norm', nn.BatchNorm1d(n_out))
-            seq.add_module('drop_out_1', nn.Dropout(0.1))
-            seq.add_module('linear_2', nn.Linear(n_out, n_out))
-            seq.add_module('relu', nn.ReLU())
-            seq.add_module('drop_out_2', nn.Dropout(0.1))
-
-            self.stage_1.append(seq)
-            self.add_module('stage_1_{}'.format(i), seq)
-
-        linear_1 = nn.Sequential()
-        linear_1.add_module('linear_1', nn.Linear(sum(stage_1_outs), 200))
-        linear_1.add_module('batchnorm_1', torch.nn.BatchNorm1d(200))
-        linear_1.add_module('relu_1', nn.ReLU())
-        linear_1.add_module('linear_2', nn.Linear(200, 100))
-        linear_1.add_module('batchnorm_2', torch.nn.BatchNorm1d(100))
-        linear_1.add_module('drop_out_2', torch.nn.Dropout(0.1))
-        linear_1.add_module('relu_2', nn.ReLU())
-        linear_1.add_module('linear_3', nn.Linear(100, 50))
-        linear_1.add_module('batchnorm_3', nn.BatchNorm1d(50))
-        linear_1.add_module('relu_3', nn.ReLU())
-        linear_1.add_module('linear_4', nn.Linear(50, 5))
-        linear_1.add_module('batchnorm_4', nn.BatchNorm1d(5))
-        self.linear_1 = linear_1
+        self.model = nn.Sequential(
+            nn.Linear(self.latent_dim, 128),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(128, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(1024, self.img_size**2),
+            nn.Tanh()
+        )
 
     def forward(self, noise):
-        x = [batch[n] for n in self.subscripted_views]
+        # import pdb;
+        img = self.model(noise)
+        # pdb.set_trace(im)
+        img = img.view(img.size()[0], self.channels, self.img_size, self.img_size)
 
-        x = [
-             [self.transform(dgm) for dgm in x[0]],
-             [reduce_essential_dgm(dgm) for dgm in x[1]],
-             [reduce_essential_dgm(dgm) for dgm in x[2]]
-            ]
+        return img
 
-        x_sl = [l(xx) for l, xx in zip(self.slayers, x)]
-
-        x = [l(xx) for l, xx in zip(self.stage_1, x_sl)]
-
-        x = torch.cat(x, 1)
-
-        x = self.linear_1(x)
-
-        return x
-
-#class Discriminator(nn.Module):
-    #def __init__(self, img_size, latent_dim):
-        #super(Discriminator, self).__init__()
-        #self.img_size = img_size
-        #self.latent_dim = latent_dim
-        #self.model = nn.Sequential(
-            #nn.Linear(self.img_size**2 + self.latent_dim, 512),
-            #nn.LeakyReLU(0.2, inplace=True),
-            #nn.Linear(512, 256),
-            #nn.LeakyReLU(0.2, inplace=True),
-            #nn.Linear(256, 1),
-            #nn.Sigmoid()
-        #)
-#
-    #def forward(self, img, latent_vector):
-        #img_flat = img.view(img.size()[0], -1)
-        #validity = self.model(torch.cat([img_flat, latent_vector],1))
-        #return validity
-
-class Topo_Decoder(nn.Module):
+class SimpleConvNet(nn.Module):
     def __init__(self, img_size, latent_dim):
-        super(Decoder, self).__init__()
+        super(SimpleConvNet, self).__init__()
         self.img_size = img_size
         self.latent_dim = latent_dim
         self.model = nn.Sequential(
@@ -176,11 +118,43 @@ class Topo_Decoder(nn.Module):
         validity = self.model(img_flat) #64x784
         return validity
 
-def train_discriminator(discriminator, imgs, latent_vector):
+def train_ConvNet(model, imgs, latent_vector):
     # imgs = imgs.view(imgs.size()[0], -1)
     # vector = torch.cat([imgs, latent_vector], 1)
     # return discriminator(vector)
-    return discriminator(imgs, latent_vector)
+    return model(imgs, latent_vector)
+
+def train_PHConvNet(model, opt, data_train, data_test):
+    optimizer = optim.SGD(model.parameters(), lr=opt.lr_start, momentum=opt.momentum)
+    loss = nn.CrossEntropyLoss()   #Todo: Change to binary cross entropy
+    trainer = Trainer(model=model,
+                      optimizer=optimizer,
+                      loss=loss,
+                      train_data=data_train,
+                      n_epochs=opt.num_epochs,
+                      cuda=torch.cuda.is_available(),
+                      variable_created_by_model=True)
+
+    def determine_lr(self, **kwargs):
+        """
+        Todo: check reference to find learning_rate
+        """
+        epoch = kwargs['epoch_count']
+        if epoch % opt.lr_step == 0:
+            return params.lr_start / 2 ** (epoch / op.lr_step)
+
+    lr_scheduler = LearningRateScheduler(determine_lr, verbose=True)
+    lr_scheduler.register(trainer)
+    progress = ConsoleBatchProgress()
+    progress.register(trainer)
+    prediction_monitor_test = PredictionMonitor(data_test,
+                                                verbose=True,
+                                                eval_every_n_epochs=1,
+                                                variable_created_by_model=True)
+    prediction_monitor_test.register(trainer)
+    trainer.prediction_monitor = prediction_monitor_test
+    return trainer
+
 
 def get_loss_discriminator(discriminator, fake_imgs, z, real_imgs, fake_z):
     adversarial_loss = nn.BCELoss()
