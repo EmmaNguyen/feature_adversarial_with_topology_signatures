b3b86417afcdf0ab4410eb38f81e696e25f33aa2
Emma Nguyen <emma.mphuong@gmail.com>
2018-07-03 23:23:42 -0400

    Multiple edits in main file:
    - Shorten code using OOP to call functions.
    - Remove unnecessary code.
    - Change name of functions.

--- run_dev_topo_ali.py
+++ run_dev_topo_ali.py
@@ -42,95 +42,39 @@ def parse_arguments():
     parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate')
     return parser.parse_args()
 
-def load_data(opt):
-    view_name_template = 'dim_0_dir_{}'
-    subscripted_views = sorted([view_name_template.format(i) for i in range(32)])
-    assert (str(len(subscripted_views)) in opt.dgm_data_path)
-    print("[ Load provider data ]")
-    dataset = Provider()
-    dataset.read_from_h5(opt.dgm_data_path)
-    assert all(view_name in dataset.view_names for view_name in subscripted_views)
-    print("[ Create data loader]")
-    data_train, data_test = train_test_from_dataset(dataset,
-                                                    test_size=opt.test_ratio,
-                                                    batch_size=opt.batch_size)
-    return data_train, data_test, subscripted_views
+opt = parse_arguments()
 
-def run_experiment(opt):
+def main():
     print("[ Start experimenting ]")
-    data_train, data_test, subscripted_views = load_data(opt)
+    data_loader = get_MNIST(opt)
+    data_train, data_test, subscripted_views = load_dgm_data(opt)
+
+    generator = ConvNetDecoder(opt.latent_dim, opt.img_size, opt.channels)
+    generator.apply(weights_init_normal)
+
+    discriminator = PHConvNet(subscripted_views)
+    discriminator.apply(weights_init_normal)
+
+    encoder = SimpleConvNetEncoder(opt.img_size, opt.latent_dim)
+    encoder.apply(weights_init_normal)
+
+    adversarial_loss = nn.BCELoss()
+
+    if cuda:
+        generator.cuda()
+        discriminator.cuda()
+        encoder.cuda()
+        adversarial_loss.cuda()
 
     for i in range(1, opt.num_experiment + 1):
         print('[ Run experiment {} ]'.format(i))
-        topo_discriminator = PHConvNet(subscripted_views)   #subscripted_views is a number of directions to reconstruct a image
-        train_topo_discriminator = train_PHConvNet(topo_discriminator, opt, data_train, data_test)
-        train_topo_discriminator.run()
-        last_ten_accuracies = list(train_topo_discriminator.prediction_monitor.accuracies.values())[-10:]
-        average_accuracy = sum(last_ten_accuracies)/ 10.0
-        export_result(opt.output_file, average_accuracy, opt.writing_mode)
+        adversarial_nets = AdversarialTopologicalLearningNets(generator, discriminator, encoder, adversarial_loss, opt)
+        adversarial_nets.train(data_loader, opt.num_epochs, opt.lr, opt.b1, opt.b2)  #Adam optimizer
+        average_ten_accuracy = adversarial_nets.top_accuracies(k=10, scoring='accuracy')/ 10.0
+        export_result(opt.output_file, average_ten_accuracy, opt.writing_mode)
+
     print("[ End experimenting ]")
 
-def run_adversarial_learning_topo_features(opt):
-        data_loader = get_MNIST(opt)
-        data_train, data_test, subscripted_views = load_data(opt)
-
-        generator = ConvNetDecoder(opt.latent_dim, opt.img_size, opt.channels)
-        generator.apply(weights_init_normal)
-        generator_solver = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-
-        discriminator = PHConvNet(subscripted_views)
-        discriminator.apply(weights_init_normal)
-        discriminator_solver = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-
-        encoder = SimpleConvNetEncoder(opt.img_size, opt.latent_dim)
-        encoder.apply(weights_init_normal)
-        encoder_solver = torch.optim.Adam(encoder.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-
-        adversarial_loss = nn.BCELoss()
-
-        if cuda:
-            generator.cuda()
-            discriminator.cuda()
-            encoder.cuda()
-            adversarial_loss.cuda()
-
-        for epoch in range(opt.num_epochs):
-            for i, (imgs, _) in enumerate(data_loader):
-
-                if cuda: imgs = imgs.type(torch.cuda.FloatTensor)
-
-                valid = Variable(Tensor(imgs.shape[0], 1).fill_(1.0), requires_grad=False)
-                fake = Variable(Tensor(imgs.shape[0], 1).fill_(0.0), requires_grad=False)
-
-                real_imgs = Variable(imgs)
-
-                generator_solver.zero_grad()
-
-                # Sample noise as generator input
-                z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
-
-                # Generate a batch of images
-                fake_imgs = generator(z)
-                fake_z = encoder(real_imgs)
-
-                generator_loss = get_loss_generator(discriminator, fake_imgs, z, real_imgs, fake_z)
-                generator_loss.backward()
-                generator_solver.step()
-
-                discriminator_solver.zero_grad()
-                discriminator_loss = get_loss_discriminator(discriminator, fake_imgs, z, real_imgs, fake_z)
-                discriminator_loss.backward()
-                discriminator_solver.step()
-
-                print ("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, opt.n_epochs, i, len(data_loader),
-                                                                discriminator_loss.data.item(), generator_loss.data.item()))
-
-                batches_done = epoch * len(data_loader) + i
-
-                if batches_done % opt.sample_interval == 0:
-                    save_image(fake_imgs.data[:25], 'images/%d.png' % batches_done, nrow=5, normalize=True)
-
 if __name__=='__main__':
-    opt = parse_arguments()
     # run_experiment(opt)
-    run_adversarial_learning_topo_features(opt)
+    main()
