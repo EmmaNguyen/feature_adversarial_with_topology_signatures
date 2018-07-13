Modify a code to calculate geom_score - Fix a part that calculate geometry score in topo GAN for pair of image instead using an representation average score for a whole batch

parallel_geometry_score/gs/geom_score.py
@@ -69,6 +69,7 @@ def geom_score(rlts1, rlts2):
        Float, a number representing topological similarity of two datasets.
 
     """
-    mrlt1 = np.mean(rlts1, axis=0)
-    mrlt2 = np.mean(rlts2, axis=0)
-    return np.sum((mrlt1 - mrlt2) ** 2)
+    #mrlt1 = np.mean(rlts1, axis=0)
+    #mrlt2 = np.mean(rlts2, axis=0)
+    #return np.sum((mrlt1 - mrlt2) ** 2)
+    return (rlts1 - rlts2) ** 2